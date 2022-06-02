8441dc14b5a17008ba97615e8058eabd7eaf6f58
RGayathiri <92334774+R-Gayathiri@users.noreply.github.com>
2022-06-01 23:07:58 -0400

    Update Lego_Data_Analysis_SQL.sql

--- Lego_Data_Analysis_SQL.sql
+++ Lego_Data_Analysis_SQL.sql
@@ -1,4 +1,3 @@
-﻿
 ----1----
 
 --What is the total number of parts per theme.
@@ -58,3 +57,33 @@ from (
 	--Order by 3 desc
 	)m
 where theme_name like '%train%'
+
+----5----
+-- What was the popular theme by year in the 21st Century
+select year, theme_name, total
+from(
+	select year, theme_name, count(set_num) as total , ROW_NUMBER() over(partition by year order by count(set_num) desc) as r
+	from dbo.analytics_main
+	where Century='21st Century'
+	and parent_theme_name is not null
+	group by year, theme_name
+)m
+where r=1
+order by year desc
+
+----6----
+--What is the most produced Color of LEGO
+
+select color_name, sum(quantity) as quantity_of_parts
+from  
+(
+select inv.color_id,inv.inventory_id,inv.part_num, cast(inv.quantity as numeric)quantity, c.name as color_name, p.name as part_name
+from inventory_parts inv
+inner join parts p
+on inv.part_num=p.part_num
+inner join colors c
+on inv.color_id=c.id
+
+)main
+group by color_name
+order by quantity_of_parts desc
